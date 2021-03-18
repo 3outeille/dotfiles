6777da46f022089e92a02b65a0c9c5b0600ebf64
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-03-18 14:33:30 +0100

    ADD uninstall.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo apt update
 
-bash setup/vim.sh
-echo "[vim]: Done."
+bash setup/vim-install.sh
+echo "[vim]: Install done."
 
 # Restart computer for zsh to be use as default shell.
-bash setup/zsh.sh
-echo "[zsh]: Done."
+bash setup/zsh-install.sh
+echo "[zsh]: Install done."
 echo "You should restart computer for zsh to be set as default shell"
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+sudo apt update
+
+bash setup/vim-uninstall.sh
+echo "[vim]: Uninstall done."
+
+bash setup/zsh-uninstall.sh
+echo "[zsh]: Uninstall done."

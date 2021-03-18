57620cd0f392f5a4a96266aacc4d602cb9117ae1
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-03-18 15:30:15 +0100

    zsh-install: cp zsh folder instead

--- setup/zsh-install.sh
+++ setup/zsh-install.sh
@@ -5,9 +5,12 @@ sudo apt install zsh
 rm $HOME/.zshrc
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
 
+cp -r $HOME/dotfiles/zsh $HOME/.zsh
+
 # Install theme
 mkdir $HOME/.zsh/themes 
-. $HOME/dotfiles/zsh/themes.sh
+. $HOME/.zsh/themes.sh
+rm $HOME/.zsh/themes.sh
 
 # Change shell to zsh
 chsh -s $(which zsh)

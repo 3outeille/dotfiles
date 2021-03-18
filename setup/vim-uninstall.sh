e7f3050f1ed82d31c4507ac172436bca63606e36
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-03-18 15:32:07 +0100

    *-uninstall.sh: Remove symbolic links

--- setup/vim-uninstall.sh
+++ setup/vim-uninstall.sh
@@ -1,2 +1,5 @@
 # Uninstall vim.
 sudo apt-get purge vim
+
+rm $HOME/.vimrc
+rm $HOME/.vim
--- setup/zsh-uninstall.sh
+++ setup/zsh-uninstall.sh
@@ -1,2 +1,6 @@
 # Install zsh
 sudo apt-get purge zsh
+
+# Remove symbolic link
+rm $HOME/.zshrc
+rm $HOME/.zsh

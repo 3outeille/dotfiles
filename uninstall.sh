9328b8db0f0ffad28fe69c78370727da54e227f9
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-03-18 16:09:19 +0100

    uninstall: Can now set shell to bash after deleting zsh

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,7 @@ sudo apt update
 . setup/vim-uninstall.sh
 echo "[vim]: Uninstall done."
 
+chsh -s $(which bash)
 . setup/zsh-uninstall.sh
 echo "[zsh]: Uninstall done."
-chsh -s $(which bash)
-$(which bash)
 echo "LOG: set bash as default shell. You should restart computer for changes to take effect."

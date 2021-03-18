0286d8458ed9f44eb63296ebf5068e54eb2b2e91
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-03-18 16:29:42 +0100

    un/install.sh: Add color to log when echo

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+MAGENTA='\e[45m'
+NC='\033[0m' # No Color
+
 sudo apt update
 
 . setup/vim-install.sh
@@ -6,4 +9,4 @@ echo "[vim]: Install done."
 # Restart computer for zsh to be use as default shell.
 . setup/zsh-install.sh
 echo "[zsh]: Install done."
-echo "LOG: You should restart computer for zsh to be set as default shell"
+echo -e "${MAGENTA}LOG: You should restart computer for zsh to be set as default shell${NC}"
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,6 @@
+MAGENTA='\e[45m'
+NC='\033[0m' # No Color
+
 chsh -s $(which bash)
 
 . setup/vim-uninstall.sh
@@ -6,4 +9,4 @@ echo "[vim]: Uninstall done."
 . setup/zsh-uninstall.sh
 echo "[zsh]: Uninstall done."
 
-echo "LOG: set bash as default shell. You should restart computer for changes to take effect."
+echo -e "${MAGENTA}LOG: set bash as default shell. You should restart computer for changes to take effect.${NC}"

abeeda0b364d7b48a62dd85646d18b053e95e145
3outeille <ferdinand.mom@epita.fr>
2021-05-23 23:14:52 +0200

    vimrc: add oceanic-material background

--- setup/vim-install.sh
+++ setup/vim-install.sh
@@ -11,15 +11,16 @@ fi
 rm $HOME/vimrc
 ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 
-# Load custome color scheme.
-mkdir -p $HOME/.vim/colors
-cp $HOME/dotfiles/vim/custom-scheme.vim $HOME/.vim/colors/custom-scheme.vim
-
 # Install all plugins
 vim +PluginInstall +qall
 
-# Modify move.vim
+# Modify vim-move/move.vim plugin
 sed -i "226,236s/'j'/'down'/g" $HOME/.vim/bundle/vim-move/plugin/move.vim
 sed -i "226,236s/'k'/'up'/g" $HOME/.vim/bundle/vim-move/plugin/move.vim
 sed -i "226,236s/'h'/'left'/g" $HOME/.vim/bundle/vim-move/plugin/move.vim
 sed -i "226,236s/'l'/'right'/g" $HOME/.vim/bundle/vim-move/plugin/move.vim
+
+# Load custom color scheme.
+mkdir -p $HOME/.vim/colors
+cp $HOME/dotfiles/vim/custom-scheme.vim $HOME/.vim/colors/custom-scheme.vim
+cp $HOME/.vim/bundle/oceanic-material/colors/oceanic_material.vim $HOME/.vim/colors/oceanic_material.vim
